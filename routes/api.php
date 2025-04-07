73ed8b7aea35bfc8b69463177f7ba803635eb391
Banana21-us <60498922+Banana21-us@users.noreply.github.com>
2025-04-07 11:32:56 +0800

    deleted:    public/assets/userPic/1742798595.jpg 	new file:   public/assets/userPic/1743036761.jpg 	modified:   routes/api.php 	modified:   app/Http/Controllers/AnnouncementController.php 	modified:   app/Http/Controllers/EmployeesController.php 	modified:   app/Http/Controllers/LeaveReqController.php 	modified:   app/Http/Controllers/NotificationController.php 	modified:   database/seeders/PositionSeeder.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -21,44 +21,24 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\WorkstatusController;
 
-// Protected Route (Requires Authentication via Sanctum)
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::post('/users', [EmployeesController::class, 'store']);
-// });
-// Route::post('/login',[EmployeesController::class,'login']);
-
-
 Route::post('/login', [EmployeesController::class, 'login'])->middleware('throttle:5,1');
 Route::post('/regusers', [EmployeesController::class, 'store']);
-// Route::middleware(['auth:sanctum'])->group(function () {
-
+Route::middleware(['auth:sanctum'])->group(function () {
 
 Route::get('/users/count', [EmployeesController::class, 'count']);
 Route::get('/leavecount', [EmployeesController::class, 'countdashadmin']);
 Route::get('/presapplieadleave', [EmployeesController::class, 'countleavepresident']);
 Route::get('/dheadapplieadleave/{id}', [EmployeesController::class, 'countleavedepthead']);
-
 Route::get('/dashboard-data/{userId}',  [EmployeesController::class, 'countTodayEventsAndApprovedLeaves']);
 Route::put('/employees/{id}', [EmployeesController::class, 'update']);
 Route::get('/employees', [EmployeesController::class, 'index']);
 Route::delete('/employees/{id}', [EmployeesController::class, 'destroy']);
 Route::get('/employees/{id}', [EmployeesController::class, 'show']);
-
 Route::put('/acceptemployees/{id}', [EmployeesController::class, 'acceptemployee']);
 Route::post('/accountsaveedit', [EmployeesController::class, 'accountsaveedit']);
-
 Route::get('/account/{userid}', [EmployeesController::class, 'getAccountDetails']);
-
 Route::apiResource('employeefamily', EmpfamilyController::class);
 Route::apiResource('spouse', SpouseController::class);
-
-Route::apiResource('employmentdetails', EmploymentdetController::class);
-
-Route::apiResource('employmenteducs', EmploymenteducController::class);
-
 
 Route::apiResource('leaverequests', LeaveReqController::class);
 Route::get('/leave-requests/dhead/{id}', [LeaveReqController::class, 'getByDHead']);
@@ -77,9 +57,10 @@ Route::put('/leave-execsec/{id}/reject', [LeaveReqController::class, 'rejectLeav
 // pres 
 Route::put('/leave-pres/{id}/approve', [LeaveReqController::class, 'approveLeaveRequestpres']);
 Route::put('/leave-pres/{id}/reject', [LeaveReqController::class, 'rejectLeaveRequestpres']);
-
 Route::get('/leave-count/{userid}', [LeaveReqController::class, 'countLeaveAndEvents']);
 
+Route::apiResource('employmentdetails', EmploymentdetController::class);
+Route::apiResource('employmenteducs', EmploymenteducController::class);
 Route::apiResource('leave-types', LeaveTypeController::class);
 Route::apiResource('department', DepartmentController::class);
 Route::apiResource('category', CategoryController::class);
@@ -93,7 +74,6 @@ Route::apiResource('requestfile', RequestfileController::class);
 Route::get('/filerecords', [RequestfileController::class, 'bynotsoa']);
 Route::get('/soarecords', [RequestfileController::class, 'bysoa']);
 Route::get('/requestfile/records/{userId}', [RequestfileController::class, 'getrecordsByUserId']);
-
 Route::post('/upload-files', [RequestfileController::class, 'uploadFiles']);
 //uploadIMG.....
 
@@ -103,7 +83,6 @@ Route::post('/notifications', [NotificationController::class, 'store']);
 Route::get('/notifications', [NotificationController::class, 'index']);
 Route::delete('/notifications/{id}', [NotificationController::class, 'destroy']);
 Route::put('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
-
 Route::get('/notifications/unread-count/{userId}', [NotificationController::class, 'getUnreadNotificationCount']);
 
 Route::apiResource('events', EventsController::class);  
@@ -131,7 +110,7 @@ Route::get('assets/userPic/{filename}', function ($filename) {
         abort(404);
 });
 
-// });
+});
 
 Route::middleware('auth:sanctum')->post('/logout',[EmployeesController::class,'logout']);
 
